7d18a179eb3bc6f8efe1038c76a0059459ebfabb
Mehmetkrdnn <122629321+mehmetkrdn@users.noreply.github.com>
2025-05-14 02:11:37 +0300

    Rest Assured testlerine göre güncellendi.

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,28 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/login', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required'
+    ]);
+
+    if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+        return response()->json([
+            'durum' => 'basarili',
+            'mesaj' => 'Giriş başarılı',
+        ], 200);
+    }
+
+    return response()->json([
+        'durum' => 'hata',
+        'mesaj' => 'Email ya da şifre hatalı',
+    ], 401);
 });
